bb0bd7c2b58394d029b2ac634c512b7aeb8bd0fd
Unknown <tolsen@limebits.com>
2008-06-30 14:23:54 -0400

    httprepo import not necessary

--- hgkerberos.py
+++ hgkerberos.py
@@ -2,8 +2,6 @@ import urllib2 as u2
 
 import urllib2_kerberos as u2k
 
-from mercurial import httprepo
-
 def reposetup(ui, repo):
     if u2._opener is not None:
         u2._opener.add_handler(u2k.HTTPKerberosAuthHandler())
